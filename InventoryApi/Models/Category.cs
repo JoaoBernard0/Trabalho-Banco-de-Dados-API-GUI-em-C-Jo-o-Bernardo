f84de1bd1fc2406a347b9605cb3eafc00032720e
JoaoBernard0 <126477093+JoaoBernard0@users.noreply.github.com>
2025-12-05 16:56:02 -0300

    Create Category.cs

---
+++ InventoryApi/Models/Category.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace InventoryApi.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = null!;
+
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
