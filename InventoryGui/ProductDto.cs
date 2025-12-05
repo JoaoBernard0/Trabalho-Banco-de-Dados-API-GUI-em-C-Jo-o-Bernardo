736cb63c7d106309096ac74869f8f157242d715a
JoaoBernard0 <126477093+JoaoBernard0@users.noreply.github.com>
2025-12-05 17:06:08 -0300

    Create ProductDto.cs

---
+++ InventoryGui/ProductDto.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace InventoryGui
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public string? CategoryName { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
